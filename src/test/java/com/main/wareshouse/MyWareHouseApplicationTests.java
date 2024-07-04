bc1b5c23a592b938f7eaaa76ec2e7d4da188a114
91740 <dev1@gmail.com>
2024-07-04 17:43:50 +0530

    this is change request1

---
+++ src/test/java/com/main/wareshouse/MyWareHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.wareshouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWareHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
